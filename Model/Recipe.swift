6a0a455b3d35841d5d248d9fcc0165cbceb07b20
Wahyu Kwan <wahyukwan@gmail.com>
2025-11-13 11:25:36 +0700

    - Add initial data model

---
+++ Model/Recipe.swift
@@ -0,0 +1,32 @@
+//
+//  Recipe.swift
+//  Recipe Book
+//
+//  Created by Moladin on 13/11/2025.
+//
+
+import Foundation
+import SwiftData
+
+class Recipe:Identifiable{
+    
+    @Attribute(.unique) var id : String
+    var name = ""
+    var difficulty = ""
+    var prepTime = 0
+    var cookTime = 0
+    var servings = 0
+    var ingredients : [String] = []
+    var instructions : [String] = []
+    var dateAdded = Date()
+    var imageData : Data?
+    var rating: Int?
+    
+    var totalTime: Int{
+        prepTime + cookTime
+    }
+    
+    init(){
+        id = UUID().uuidString
+    }
+}
